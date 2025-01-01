5bdde6848c8173b89cb7243a700eedce230bb51a
rafrodvel <147498958+rafrodvel@users.noreply.github.com>
2025-01-01 16:41:32 +0100

    Add files via upload

--- galsim.c
+++ galsim.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <omp.h>
+#include <stdbool.h>
 
 #define EPSILON_O 1e-3
 
@@ -20,92 +21,67 @@ static double get_wall_seconds() {
 // Structure for the tree
 typedef struct node_t {
     int particle_index;
-    int has_particle;
-    int has_children;
-    double min_x, max_x, min_y, max_y, node_mass, cm_x, cm_y;
-    struct node_t* children;
+    bool is_leaf;
+    double x, y, width, node_mass, cm_x, cm_y;
+    struct node_t* children[4];
 } node_t;
 
 void update_mass_and_cm(node_t* node, int particle);
-node_t* get_child(node_t* node, int particle);
-void subdivide(node_t* node);
+void get_size(node_t* node, int particle, double* dimensions, int* index);
 
 /* ------------------------------
             Tree assembly
    ------------------------------ */
 
-void insert(node_t* node, int particle) {
-    // If the node is empty, insert the particle and set the mass and center of mass
-    if (!node->has_particle) {
-        node->particle_index = particle;
-        node->cm_x = pos_x[particle];
-        node->cm_y = pos_y[particle];
-        node->node_mass = mass[particle];
-        node->has_particle = 1;
+node_t* create_node(double x, double y, double width) {
+    node_t* node = malloc(sizeof(node_t));
+    node->x = x;
+    node->y = y;
+    node->width = width;
+    for (int i = 0; i < 4; i++) {
+        node->children[i] = NULL;
+    }
+    return node;
+}
 
-    // If the node has children, insert the particle in the appropriate child
-    } else if (node->has_children) {
-        update_mass_and_cm(node, particle);
-        insert(get_child(node, particle), particle);
+void insert(node_t** node, int particle, double x, double y, double width) {
+    // If there's no node, create it and add particle
+    if (*node == NULL) {
+        *node = create_node(x, y, width);
+        (*node)->particle_index = particle;
+        (*node)->cm_x = pos_x[particle];
+        (*node)->cm_y = pos_y[particle];
+        (*node)->node_mass = mass[particle];
+        (*node)->is_leaf = true;
 
-    // If the node is a leaf, subdivide it and insert the old and new particles
+    // If there is, set leaf to false and add old and new particles
     } else {
-        node->has_children = 1;
-        subdivide(node);
-        insert(get_child(node, node->particle_index), node->particle_index); // Old
-        insert(get_child(node, particle), particle); // New
-        update_mass_and_cm(node, particle);
+        if ((*node)->is_leaf) {
+            (*node)->is_leaf = false;
+            int index;
+            double dimensions[3]; // x, y, width
+            get_size(*node, (*node)->particle_index, dimensions, &index);
+            insert(&(*node)->children[index], (*node)->particle_index, dimensions[0], dimensions[1], dimensions[2]); // Old
+        } 
+        int index;
+        double dimensions[3]; // x, y, width
+        get_size(*node, particle, dimensions, &index);
+        insert(&(*node)->children[index], particle, dimensions[0], dimensions[1], dimensions[2]); // New
+        update_mass_and_cm(*node, particle);
     }
 }
 
-void subdivide(node_t* node) {
-    // A few variable names for readability
-    double min_x = node->min_x;
-    double max_x = node->max_x;
-    double min_y = node->min_y;
-    double max_y = node->max_y;
-    double mid_x = (min_x + max_x) / 2;
-    double mid_y = (min_y + max_y) / 2;
-
-    // Allocate memory for the children
-    node->children = malloc(4 * sizeof(node_t));
-
-    // Initialize the children
-    for (int i = 0; i < 4; i++) { 
-        node->children[i].has_particle = 0;
-        node->children[i].has_children = 0;
-    }
-
-    node->children[0].min_x = min_x;
-    node->children[0].max_x = mid_x;
-    node->children[0].min_y = min_y;
-    node->children[0].max_y = mid_y;
-
-    node->children[1].min_x = min_x;
-    node->children[1].max_x = mid_x;
-    node->children[1].min_y = mid_y;
-    node->children[1].max_y = max_y;
-
-    node->children[2].min_x = mid_x;
-    node->children[2].max_x = max_x;
-    node->children[2].min_y = min_y;
-    node->children[2].max_y = mid_y;
-
-    node->children[3].min_x = mid_x;
-    node->children[3].max_x = max_x;
-    node->children[3].min_y = mid_y;
-    node->children[3].max_y = max_y;
+void get_size(node_t* node, int particle, double* dimensions, int* index) {
+    double mid_x = node->x + node->width / 2;
+    double mid_y = node->y + node->width / 2;
+    // Clever way to get the index of the child
+    *index = (pos_x[particle] >= mid_x) *2 | (pos_y[particle] >= mid_y);
+    
+    dimensions[2] = node->width / 2;
+    dimensions[0] = (*index < 2) ? node->x : mid_x;
+    dimensions[1] = (*index % 2 == 0) ? node->y : mid_y;
 }
 
-// Function to get the child of a node that a particle belongs to
-node_t* get_child(node_t* node, int particle) {
-    double mid_x = (node->min_x + node->max_x) / 2;
-    double mid_y = (node->min_y + node->max_y) / 2;
-    // Clever way to get the index of the child: it compares pairs of bits of left and right outcomes
-    // so that 0 | 0 = 0,   0 | 1 = 1,   2 | 0 = 2,    2 | 1 = 3
-    int index = (pos_x[particle] >= mid_x) *2 | (pos_y[particle] >= mid_y);
-    return &node->children[index];
-}
 
 void update_mass_and_cm(node_t* node, int particle) {
     double total_mass = node->node_mass + mass[particle];
@@ -114,52 +90,63 @@ void update_mass_and_cm(node_t* node, int particle) {
     node->node_mass = total_mass;
 }
 
+// Clean memory
+void free_tree(node_t** node) {
+    if (*node == NULL) {
+        return;
+    }
+    if ((*node)->is_leaf == false) {
+        for (int i = 0; i < 4; i++) {
+            free_tree(&(*node)->children[i]);
+        }
+    }
+    free(*node);
+    *node = NULL;
+}
+
 /* ------------------------------
          Force calculation
    ------------------------------ */
 
-void calculate_force(int particle_index, node_t* node) {
-    double r_x = pos_x[particle_index] - node->cm_x;
-    double r_y = pos_y[particle_index] - node->cm_y;
+void get_r(node_t* node, int particle, double* distances) {
+    double r_x = pos_x[particle] - node->cm_x;
+    double r_y = pos_y[particle] - node->cm_y;
     double r_squared = r_x * r_x + r_y * r_y;
-    double r_plummer = sqrt(r_squared) + EPSILON_O;
+    distances[0] = r_x;
+    distances[1] = r_y;
+    distances[2] = sqrt(r_squared);
+}
+
+void calculate_force(int particle_index, node_t* node, double distances[3]) {
+    
+    double r_plummer = distances[2] + EPSILON_O;
     double r_cubed = r_plummer * r_plummer * r_plummer;
     double force_factor = -G * mass[particle_index] * node->node_mass / r_cubed;
 
-    fx[particle_index] += force_factor * r_x;
-    fy[particle_index] += force_factor * r_y;
+    fx[particle_index] += force_factor * distances[0];
+    fy[particle_index] += force_factor * distances[1];
 }
 
 void update_forces(int particle, node_t* node) {
-    // If the node has no children and has a particle that is not the one we are calculating the force on, calculate the force
-    if (!node->has_children && node->has_particle && node->particle_index != particle) {
-        calculate_force(particle, node);
-    } 
-    // If the node has children, check theta and decide whether to use it or its children
-    else if (node->has_children) {
-        double r_x = pos_x[particle] - node->cm_x;
-        double r_y = pos_y[particle] - node->cm_y;
-        double r_squared = r_x * r_x + r_y * r_y;
-        double theta = (node->max_x - node->min_x) / sqrt(r_squared);
+    // If the node has a particle that is not the one we are calculating the force on, calculate the force
+    double distances[3]; // x, y, r
+    get_r(node, particle, distances);
+    
+    if (node->is_leaf) {
+        calculate_force(particle, node, distances);
+    } else {
+        double theta = node->width / distances[2];
 
         if (theta < theta_0) {
-            calculate_force(particle, node);
-        } 
-        else {
+            calculate_force(particle, node, distances);
+        } else {
             for (int i = 0; i < 4; i++) {
-                update_forces(particle, &node->children[i]);
+                if (node->children[i] != NULL) {
+                    update_forces(particle, node->children[i]);
+                }
             }
         }
-    }
-}
-
-// Function to free the tree's children
-void free_tree(node_t* node) {
-    if (node->has_children) {
-        for (int i = 0; i < 4; i++) {
-            free_tree(&node->children[i]);
-        }
-        free(node->children);
+    
     }
 }
 
@@ -170,12 +157,9 @@ void free_tree(node_t* node) {
 int main(int argc, char* argv[]) {
     double time_tol = get_wall_seconds();
 
-    // Set number of threads
-    omp_set_num_threads(8);
-
-    if (argc != 6) {
+    if (argc != 7) {
         printf("You should enter the following parameters in order:\n");
-        printf("N filename nsteps delta_t theta_0\n");
+        printf("N filename nsteps delta_t theta_0 processes\n");
         return 1;
     }
 
@@ -184,6 +168,9 @@ int main(int argc, char* argv[]) {
     int nsteps = atoi(argv[3]);
     double delta_t = atof(argv[4]);
     theta_0 = atof(argv[5]);
+    int processes = atoi(argv[6]);
+
+    omp_set_num_threads(processes);
 
     FILE* data_file = fopen(filename, "rb");
     if (data_file == NULL) {
@@ -205,6 +192,7 @@ int main(int argc, char* argv[]) {
     double ay[N];
     double brightness[N];
     G = 100.0 / N;
+    double half_dt_squared = 0.5 * delta_t * delta_t;
 
     // Read data from file
     for (int i = 0; i < N; i++) {
@@ -222,19 +210,13 @@ int main(int argc, char* argv[]) {
     /* Calculate first step acceleration for Verlet Velocity */
 
     // Create root node
-    node_t* root = malloc(sizeof(node_t));
-    root->has_particle = 0;
-    root->has_children = 0;
-    root->min_x = 0.0;
-    root->max_x = 1.0;
-    root->min_y = 0.0;
-    root->max_y = 1.0;
+    node_t* root = NULL;
 
     // Initialize forces and insert particles
     for (int i = 0; i < N; i++) {
         fx[i] = 0;
         fy[i] = 0;
-        insert(root, i);
+        insert(&root, i, 0, 0, 1);
     }
 
     // Calculate forces
@@ -244,8 +226,7 @@ int main(int argc, char* argv[]) {
     }
 
     // Free tree
-    free_tree(root);
-    free(root);
+    free_tree(&root);
     
     // Time iterations
     for (int step = 0; step < nsteps; step++) {
@@ -255,25 +236,21 @@ int main(int argc, char* argv[]) {
         for (int i = 0; i < N; i++) {
             ax[i] = fx[i] * mass_inver[i];
             ay[i] = fy[i] * mass_inver[i];
-            pos_x[i] += delta_t * vx[i] + 0.5 * delta_t * delta_t * ax[i];
-            pos_y[i] += delta_t * vy[i] + 0.5 * delta_t * delta_t * ay[i];
+            pos_x[i] += delta_t * vx[i] + half_dt_squared * ax[i];
+            pos_y[i] += delta_t * vy[i] + half_dt_squared * ay[i];
         }
 
-        // Create root, initialize, insert particles and update forces
-        root = malloc(sizeof(node_t));
-        root->has_particle = 0;
-        root->has_children = 0;
-        root->min_x = 0.0;
-        root->max_x = 1.0;
-        root->min_y = 0.0;
-        root->max_y = 1.0;
+        // Create root node
+        node_t* root = NULL;
 
+        // Initialize forces and insert particles
         for (int i = 0; i < N; i++) {
             fx[i] = 0;
             fy[i] = 0;
-            insert(root, i);
+            insert(&root, i, 0, 0, 1);
         }
 
+        // Calculate forces
         #pragma omp parallel for
         for (int i = 0; i < N; i++) {
             update_forces(i, root);
@@ -286,8 +263,7 @@ int main(int argc, char* argv[]) {
             vy[i] += 0.5 * delta_t * (fy[i] * mass_inver[i] + ay[i]);
         }
 
-        free_tree(root);
-        free(root);
+        free_tree(&root);
 
     }
 
